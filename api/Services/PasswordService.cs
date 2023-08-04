0d9e80fbfbed245f9a17e8308695a5cd0c9af765
John Brian <njugunajb96@gmail.com>
2023-08-04 13:05:22 +0300

    create password interface implementation

---
+++ api/Services/PasswordService.cs
@@ -0,0 +1,28 @@
+﻿using api.Interfaces;
+using BC = BCrypt.Net.BCrypt;
+
+namespace api.Services;
+
+public class PasswordService:IPasswordService
+{
+    /// <summary>
+    /// Creates a password hash
+    /// </summary>
+    /// <param name="rawPassword">The password to hash</param>
+    /// <returns>The hashed password</returns>
+    public string CreatePasswordHash(string rawPassword)
+    {
+        return BC.HashPassword(rawPassword);
+    }
+
+    /// <summary>
+    /// Compares the hashed password with the un-hashed password
+    /// </summary>
+    /// <param name="rawPassword">un-hashed password</param>
+    /// <param name="hashedPassword">hashed password</param>
+    /// <returns>true if password matched or false</returns>
+    public bool PasswordIsValid(string rawPassword, string hashedPassword)
+    {
+        return BC.Verify(rawPassword, hashedPassword);
+    }
+}
