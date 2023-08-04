8c2766d600d6688747e8320219949b6c91da195f
John Brian <njugunajb96@gmail.com>
2023-08-04 11:48:14 +0300

    create user table config

---
+++ api/Database/EntityConfigs/UserConfig.cs
@@ -0,0 +1,14 @@
+﻿using api.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace api.Database.EntityConfigs;
+
+public class UserConfig : IEntityTypeConfiguration<UserEntity>
+{
+    public void Configure(EntityTypeBuilder<UserEntity> builder)
+    {
+        builder.ToTable("Users");
+        builder.Property(b => b.RowVersion);
+    }
+}
