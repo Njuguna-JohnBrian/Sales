9be3c247cf13cd7cbe54630ba04e36274a2f8ee9
John Brian <njugunajb96@gmail.com>
2023-08-04 11:45:38 +0300

    add auth service for abstraction

---
+++ api/Services/AuthService.cs
@@ -0,0 +1,24 @@
+﻿using api.Database;
+using api.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Services;
+
+public class AuthService : IAuthService
+
+{
+    private readonly DatabaseContext _context;
+
+    public AuthService(DatabaseContext databaseContext)
+    {
+        _context = databaseContext;
+    }
+
+    public async Task<bool> UserExistsAsync(string email)
+    {
+        var userExists =
+            await _context.UserEntities.FirstOrDefaultAsync(entity => entity.Email == email);
+
+        return userExists != null;
+    }
+}
