3e1928144b3f1f214293f7fc5f9b9dcfa2e7bfbb
John Brian <njugunajb96@gmail.com>
2023-08-04 11:46:02 +0300

    create db context

---
+++ api/Database/DatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using api.Database.Entities;
+using api.Database.EntityConfigs;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Database;
+
+public class DatabaseContext : DbContext
+{
+    public DatabaseContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+    {
+    }
+
+
+    public virtual DbSet<UserEntity> UserEntities { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfiguration(new UserConfig());
+    }
+}
