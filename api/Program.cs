a846cc15b051cc9a3363dac10f2140e35101b77d
John Brian <njugunajb96@gmail.com>
2023-08-04 11:47:17 +0300

    add DI for swagger and services

--- api/Program.cs
+++ api/Program.cs
@@ -1,19 +1,67 @@
+using api.Database;
+using api.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddScoped<AuthService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddDbContext<DatabaseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add swagger
+builder.Services.AddSwaggerGen(swaggerGen =>
+{
+    swaggerGen.SwaggerDoc("v1", new OpenApiInfo { Title = "JiliMall Api", Version = "v1" });
+    swaggerGen.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please provide token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
+    swaggerGen.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "JiliMall Api");
+
+        // serve swagger in the root api endpoint
+        options.RoutePrefix = string.Empty;
+    });
+
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
