38ebd28c3767357fa4b070a9ce37abd23a12284b
John Brian <njugunajb96@gmail.com>
2023-08-07 20:34:02 +0300

    set up token service methods

---
+++ api/Interfaces/ITokenService.cs
@@ -0,0 +1,7 @@
+﻿namespace api.Interfaces;
+
+public interface ITokenService
+{
+    public string CreateToken(dynamic targetEntity, List<string> claimTarget);
+    public string GetClaimValue(dynamic targetEntity, string target);
+}
