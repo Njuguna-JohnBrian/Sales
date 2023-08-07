1b2e23ee79a4a8bd21cdbe40785eb7b2c3cfce5d
John Brian <njugunajb96@gmail.com>
2023-08-07 23:54:53 +0300

    token interface

--- api/Interfaces/ITokenService.cs
+++ api/Interfaces/ITokenService.cs
@@ -2,6 +2,6 @@
 
 public interface ITokenService
 {
-    public string CreateToken(dynamic targetEntity, List<string> claimTarget);
-    public string GetClaimValue(dynamic targetEntity, string target);
+    public string CreateToken<TEntity>(TEntity entity, List<string> claimTarget) where TEntity : class;
+    public string GetClaimValue<TEntity>(TEntity entity, string target) where TEntity : class;
 }
