c94d688ecc47e1d06a5c02f191cc137cc7e8af66
John Brian <njugunajb96@gmail.com>
2023-09-01 12:47:49 +0300

    update token service unit tests

--- api.tests/Systems/Services/TestTokenService.cs
+++ api.tests/Systems/Services/TestTokenService.cs
@@ -9,17 +9,14 @@ public class TestTokenService
     private readonly Mock<IConfiguration> _mockConfiguration = new();
     private readonly TokenService _tokenService;
 
-    private static readonly Dictionary<string, object> TestEntity = new()
+    private static readonly UserEntity TestEntity = new()
     {
-        { "Id", "1" },
-        { "FirstName", "testFirstName" },
-        { "LastName", "testLastName" }
+        Id = 1,
+        FirstName = "testUser",
+        LastName = "testUser",
+        Email = "test@email.com",
+        PasswordHash = "$2a$11$8STxovb1zSpiMqGkxOcJbOkKvvUTYWWEE2dW92mmpZ1JvPhDUJwjO"
     };
-
-    private static readonly List<string> ClaimTargets = new() { "Id", "FirstName", "LastName" };
-
-
-    private readonly UserEntity? _nullUserEntity = null;
 
     private const string TestToken = "thisCannotBe21u94712-wordQHacked3242You82374can82937Try";
     private const string TokenConfigName = "Token";
@@ -35,7 +32,7 @@ public class TestTokenService
     [Fact]
     public void CreateToken_Returns_A_String_And_Is_Not_Null()
     {
-        var jwtToken = _tokenService.CreateToken(TestEntity, ClaimTargets);
+        var jwtToken = _tokenService.CreateToken(TestEntity);
 
         jwtToken.Should().NotBeNullOrEmpty();
         jwtToken.Should().BeOfType<string>();
@@ -44,21 +41,10 @@ public class TestTokenService
     [Fact]
     public void CreateToken_Returns_A_Valid_Token()
     {
-        var jwtToken = _tokenService.CreateToken(TestEntity, ClaimTargets);
+        var jwtToken = _tokenService.CreateToken(TestEntity);
         var tokenHandler = new JwtSecurityTokenHandler();
         var isValidToken = tokenHandler.CanReadToken(jwtToken);
 
         isValidToken.Should().BeTrue();
-    }
-
-    [Fact]
-    public void CreateToken_NullTargetEntity_Throws_NullException()
-    {
-#pragma warning disable CS8634 // The type cannot be used as type parameter in the generic type or method. Nullability of type argument doesn't match 'class' constraint.
-        Action act = () => _tokenService.CreateToken(_nullUserEntity, ClaimTargets);
-#pragma warning restore CS8634 // The type cannot be used as type parameter in the generic type or method. Nullability of type argument doesn't match 'class' constraint.
-
-
-        act.Should().Throw<ArgumentNullException>().WithParameterName("entity");
     }
 }
