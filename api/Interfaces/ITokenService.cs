edb3bd0de3c67391a58e9622f23e79386b6cfc72
John Brian <njugunajb96@gmail.com>
2023-09-01 12:46:26 +0300

    update token service interface

--- api/Interfaces/ITokenService.cs
+++ api/Interfaces/ITokenService.cs
@@ -1,7 +1,8 @@
-﻿namespace api.Interfaces;
+﻿using api.Database.Entities;
+
+namespace api.Interfaces;
 
 public interface ITokenService
 {
-    public string CreateToken<TEntity>(TEntity entity, List<string> claimTarget) where TEntity : class;
-    public string GetClaimValue<TEntity>(TEntity entity, string target) where TEntity : class;
+    public string CreateToken(UserEntity userEntity);
 }
