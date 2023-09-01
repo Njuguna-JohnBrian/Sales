7b8467e66b96bb1922ed4f11afb1a2f0ecd92a0d
John Brian <njugunajb96@gmail.com>
2023-09-01 12:45:55 +0300

    create login dto

--- api/DTOs/LoginDTO.cs
+++ api/DTOs/LoginDTO.cs
@@ -1,6 +1,12 @@
-﻿namespace api.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
 
-public class LoginDTO
+namespace api.DTOs;
+
+public class LoginDto
 {
-    
+    [Required(ErrorMessage = "Email is required"), EmailAddress(ErrorMessage = "Email is invalid")]
+    public required string Email { get; set; }
+
+    [Required(ErrorMessage = "Password is required")]
+    public required string Password { get; set; }
 }
