616449d5729abac1aae8ef59a6e379391695a65e
John Brian <njugunajb96@gmail.com>
2023-09-07 10:03:19 +0300

    update seed data

--- JiliMallDb/dbo/scripts/seed-data.sql
+++ JiliMallDb/dbo/scripts/seed-data.sql
@@ -5,51 +5,28 @@
 SET
     IDENTITY_INSERT [dbo].[Users] ON;
 ;
-WITH usersSeed
-    AS
-    (SELECT *
-     FROM (VALUES (1,
-                   N'd4a98758-994e-47c0-8d3c-73b4738d3cdb',
-                   N'Test',
-                   N'Test',
-                   N'test@email.com',
-                   N'$2a$11$kBL9t.CA3e4I3kGum5GisOQ1aLhy3V.RpCs21MoYYT70fZb6reoqy',
-                   GETDATE())) val([Id], [UserId], [FirstName], [LastName], [Email], [PasswordHash], [RegistrationDTM]))
+WITH
+   usersSeed
+   AS
+   (
+      SELECT *
+      FROM (VALUES
+            (1,
+               N'd4a98758-994e-47c0-8d3c-73b4738d3cdb',
+               N'Test',
+               N'Test',
+               N'test@email.com',
+               N'$2a$11$kBL9t.CA3e4I3kGum5GisOQ1aLhy3V.RpCs21MoYYT70fZb6reoqy',
+               GETDATE())) val([Id], [UserId], [FirstName], [LastName], [Email], [PasswordHash], [RegistrationDTM])
+   )
     MERGE [dbo].[Users] AS Target
 USING usersSeed AS Source
-ON Source.ID = Target.ID
+ON Source.Id = Target.Id
 WHEN NOT MATCHED BY Target THEN
     INSERT
-    ([
-Id
-]
-        ,
-     [
-     UserId
-     ]
-        ,
-     [
-     FirstName
-     ]
-        ,
-     [
-     LastName
-     ]
-        ,
-     [
-     Email
-     ]
-        ,
-     [
-     PasswordHash
-     ]
-        ,
-     [
-     RegistrationDTM
-     ])
-    VALUES (Source.[Id], Source.[UserId], Source.[FirstName], Source.[LastName], Source.[Email], Source.[PasswordHash],
-            Source.[RegistrationDTM])
-    ;
+    ([Id],[UserId],[FirstName],[LastName],[Email],[PasswordHash],[RegistrationDTM])
+    VALUES (Source.[Id], Source.[UserId], Source.[FirstName], Source.[LastName], Source.[Email], Source.[PasswordHash],Source.[RegistrationDTM])
+;
 
 SET
     IDENTITY_INSERT [dbo].[Users] OFF
@@ -59,39 +36,29 @@ SET
 
 SET IDENTITY_INSERT [dbo].[Roles] ON;
 ;
-WITH rolesSeed
-    AS
-    (SELECT *
-     FROM (VALUES (1,
-                   N'd4a98758-994e-47c0-8d3c-73b4738d3cdb',
-                   N'Admin',
-                   N'Has all access'),
-                  (2,
-                   N'd4a98758-995e-47c0-8d3c-73b4738d3cdb',
-                   N'User',
-                   N'Has normal access')) val([Id], [RoleId], [RoleName], [RoleDescription]))
+WITH
+   rolesSeed
+   AS
+   (
+      SELECT *
+      FROM (VALUES
+            (1,
+               N'd4a98758-994e-47c0-8d3c-73b4738d3cdb',
+               N'Admin',
+               N'Has all access'),
+            (2,
+               N'd4a98758-995e-47c0-8d3c-73b4738d3cdb',
+               N'User',
+               N'Has normal access')) val([Id], [RoleId], [RoleName], [RoleDescription])
+   )
     MERGE [dbo].[Roles] AS Target
 USING rolesSeed AS Source
-ON Source.ID = Target.ID
+ON Source.Id = Target.Id
 WHEN NOT MATCHED BY Target THEN
     INSERT
-    ([
-Id
-]
-        ,
-     [
-     RoleId
-     ]
-        ,
-     [
-     RoleName
-     ]
-        ,
-     [
-     RoleDescription
-     ])
+    ([Id],[RoleId],[RoleName],[RoleDescription])
     VALUES (Source.[Id], Source.[RoleId], Source.[RoleName], Source.[RoleDescription])
-    ;
+;
 
 SET
     IDENTITY_INSERT [dbo].[Roles] OFF
