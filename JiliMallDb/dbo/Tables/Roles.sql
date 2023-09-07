6af1ad169c41f06cd494c79d6a8a2b2e3378b686
John Brian <njugunajb96@gmail.com>
2023-09-07 08:36:11 +0300

    create roles table

--- JiliMallDb/dbo/Tables/Roles.sql
+++ JiliMallDb/dbo/Tables/Roles.sql
@@ -1,17 +1,17 @@
 CREATE TABLE [dbo].[Roles]
 (
-  [Id] BIGINT IDENTITY (1, 1) NOT NULL,
-  [RoleId] UNIQUEIDENTIFIER NOT NULL DEFAULT (NEWID()),
-  [RoleName] NVARCHAR(20) NOT NULL,
-  [RoleDescription] NVARCHAR(200) NOT NULL,
-  [CreatedBy] BIGINT NULL,
-  [CreatedDTM] DATETIME NULL,
-  [UpdatedBy] BIGINT NULL,
-  [UpdatedDTM] BIGINT NULL,
-  [RowVersion] ROWVERSION,
-  CONSTRAINT PK_Roles_Id PRIMARY KEY CLUSTERED ([Id]),
-  CONSTRAINT [FK_Projects_Roles_CreatedBy] FOREIGN KEY ([CreatedBy]) REFERENCES [dbo].[Users]  ([Id]),
-  CONSTRAINT [FK_Projects_Roles_UpdatedBy] FOREIGN KEY ([UpdatedBy]) REFERENCES [dbo].[Users]  ([Id]),
+    [Id]              BIGINT IDENTITY (1, 1) NOT NULL,
+    [RoleId]          UNIQUEIDENTIFIER       NOT NULL DEFAULT (NEWID()),
+    [RoleName]        NVARCHAR(20)           NOT NULL,
+    [RoleDescription] NVARCHAR(200)          NOT NULL,
+    [CreatedBy]       BIGINT                 NULL,
+    [CreatedDTM]      DATETIME               NULL,
+    [UpdatedBy]       BIGINT                 NULL,
+    [UpdatedDTM]      BIGINT                 NULL,
+    [RowVersion]      ROWVERSION,
+    CONSTRAINT PK_Roles_Id PRIMARY KEY CLUSTERED ([Id]),
+    CONSTRAINT [FK_Users_Roles_CreatedBy] FOREIGN KEY ([CreatedBy]) REFERENCES [dbo].[Users] ([Id]),
+    CONSTRAINT [FK_Users_Roles_UpdatedBy] FOREIGN KEY ([UpdatedBy]) REFERENCES [dbo].[Users] ([Id]),
 
 );
 GO
@@ -20,10 +20,10 @@ CREATE NONCLUSTERED INDEX IX_Roles_RoleId
     ON [dbo].[Roles] ([RoleId])
 GO
 
-CREATE NONCLUSTERED INDEX UQ_Roles_RoleName
+CREATE UNIQUE INDEX UQ_Roles_RoleName
     ON [dbo].[Roles] ([RoleName])
 GO
 
-CREATE NONCLUSTERED INDEX UQ_Roles_RoleDescription
+CREATE UNIQUE INDEX UQ_Roles_RoleDescription
     ON [dbo].[Roles] ([RoleDescription])
 GO
