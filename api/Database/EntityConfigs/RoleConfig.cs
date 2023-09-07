01501ff8ce51cfb77b2307f1ff9dee457f001006
John Brian <njugunajb96@gmail.com>
2023-09-07 08:34:25 +0300

    configure roles table and fk

---
+++ api/Database/EntityConfigs/RoleConfig.cs
@@ -0,0 +1,21 @@
+﻿using api.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace api.Database.EntityConfigs;
+
+public class RoleConfig : IEntityTypeConfiguration<RoleEntity>
+{
+    public void Configure(EntityTypeBuilder<RoleEntity> builder)
+    {
+        builder.ToTable("Roles");
+        builder.Property(b => b.RowVersion);
+        builder.HasOne<UserEntity>(s => s.CreatedByUserEntity)
+            .WithMany()
+            .HasForeignKey(r => r.CreatedBy);
+
+        builder.HasOne<UserEntity>(s => s.UpdatedByUserEntity)
+            .WithMany()
+            .HasForeignKey(r => r.UpdatedBy);
+    }
+}
