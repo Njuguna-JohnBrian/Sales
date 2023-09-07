0b55d598053f151cd4f39bed49189ec885d69321
John Brian <njugunajb96@gmail.com>
2023-09-07 08:34:42 +0300

    create role entity

---
+++ api/Database/Entities/RoleEntity.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api.Database.Entities;
+
+public sealed class RoleEntity
+{
+    public long Id { get; set; }
+    public Guid RoleId { get; set; }
+    public required string RoleName { get; set; }
+    public required string RoleDescription { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime? CreatedDtm { get; set; }
+    public string? UpdatedBy { get; set; }
+    public DateTime? UpdatedDtm { get; set; }
+#pragma warning disable CS8618
+    [Timestamp] public byte[] RowVersion { get; set; }
+
+    public UserEntity CreatedByUserEntity { get; set; }
+    public UserEntity UpdatedByUserEntity { get; set; }
+}
