4a8cfabd85a7b2de8a04e4e4ca5e7b8e0e0b03b0
John Brian <njugunajb96@gmail.com>
2023-09-15 10:37:07 +0300

    create service to get roles

---
+++ api/Services/RoleService.cs
@@ -0,0 +1,25 @@
+﻿using api.Database;
+using api.Database.Entities;
+using api.Interfaces;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Services;
+
+public class RoleService : IRoleService
+{
+    private readonly DatabaseContext _context;
+
+    public RoleService(DatabaseContext databaseContext)
+    {
+        _context = databaseContext;
+    }
+
+
+    public async Task<List<RoleEntity>> GetRolesAsync()
+    {
+        var roles = await _context.RoleEntities.ToListAsync();
+        return roles;
+    }
+}
