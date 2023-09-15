3a7693beac0e0cc06903989d49369c7b2618da6e
John Brian <njugunajb96@gmail.com>
2023-09-15 20:46:58 +0300

    finish role dto

--- api/DTOs/RoleDTO.cs
+++ api/DTOs/RoleDTO.cs
@@ -1,6 +1,12 @@
-﻿namespace api.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
 
-public class RoleDTO
+namespace api.DTOs;
+
+public class RoleDto
 {
-    
+    [Required(ErrorMessage = "Role name is required")]
+    public required string RoleName { get; set; }
+
+    [Required(ErrorMessage = "Role description is required")]
+    public required string RoleDescription { get; set; }
 }
