e7d0a89ba6acc04ecec4cc03c46cce162707554e
John Brian <njugunajb96@gmail.com>
2023-09-17 08:40:47 +0300

    update type mismatch

--- api/Database/Entities/RoleEntity.cs
+++ api/Database/Entities/RoleEntity.cs
@@ -8,9 +8,9 @@ public sealed class RoleEntity
     public Guid RoleId { get; set; }
     public required string RoleName { get; set; }
     public required string RoleDescription { get; set; }
-    public string? CreatedBy { get; set; }
+    public long? CreatedBy { get; set; }
     public DateTime? CreatedDtm { get; set; }
-    public string? UpdatedBy { get; set; }
+    public long? UpdatedBy { get; set; }
     public DateTime? UpdatedDtm { get; set; }
 #pragma warning disable CS8618
     [Timestamp] public byte[] RowVersion { get; set; }
