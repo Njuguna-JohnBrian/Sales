5a4cbdb63b85415bdbd9755e8e66d0b5a5b5c82d
John Brian <njugunajb96@gmail.com>
2023-09-17 09:49:26 +0300

    add more methods to interface

--- api/Interfaces/IRoleService.cs
+++ api/Interfaces/IRoleService.cs
@@ -6,6 +6,10 @@ namespace api.Interfaces;
 
 public interface IRoleService
 {
+    Task<RoleEntity?> RoleExists(string roleName);
     Task<List<RoleEntity>> GetRoles();
     Task<RoleEntity> AddRole(RoleDto roleDto, HttpContext httpContext);
+    Task<RoleEntity> UpdateRole(UpdateRoleDto updateRoleDto, 
+        RoleEntity roleEntity, HttpContext httpContext
+        );
 }
