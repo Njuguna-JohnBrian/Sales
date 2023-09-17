2c5e64f6ae8981be8bd2294786e66616b848f04c
John Brian <njugunajb96@gmail.com>
2023-09-17 09:49:53 +0300

    create update role dto

--- api/DTOs/RoleDTO.cs
+++ api/DTOs/RoleDTO.cs
@@ -10,3 +10,15 @@ public class RoleDto
     [Required(ErrorMessage = "Role description is required")]
     public required string RoleDescription { get; set; }
 }
+
+public class UpdateRoleDto
+{
+    [Required(ErrorMessage = "Role name is required")]
+    public required string RoleName { get; set; }
+
+    [Required(ErrorMessage = "Role description is required")]
+    public required string RoleDescription { get; set; }
+    
+    [Required(ErrorMessage = "Role id is required")]
+    public required Guid RoleId { get; set; }
+}
