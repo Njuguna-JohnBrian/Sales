746263435323b617332af5fff585a5c6980cfd01
John Brian <njugunajb96@gmail.com>
2023-09-18 21:28:55 +0300

    test master

--- api.tests/Systems/Services/TestRoleService.cs
+++ api.tests/Systems/Services/TestRoleService.cs
@@ -1,7 +1,9 @@
 ﻿using api.Database;
 using api.Database.Entities;
 using api.tests.Helpers;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Net.Http.Headers;
 using Moq.EntityFrameworkCore;
 
 namespace api.tests.Systems.Services;
@@ -9,15 +11,41 @@ namespace api.tests.Systems.Services;
 public class TestRoleService
 {
     private readonly Mock<IConfiguration> _mockConfiguration = new();
+    private readonly TokenService _tokenService;
+    private readonly string _jwtToken;
+    private IRoleService RoleService { get; }
+    private readonly Mock<HttpRequest> _mockHttpRequest = new();
+    private readonly Mock<HttpContext> _mockHttpContext = new();
+    private const string TokenConfigName = "Token";
+    private const string TestToken = "thisCannotBe21u94712-wordQHacked3242You82374can82937Try";
 
-    private IRoleService RoleService { get; set; }
+    private static readonly UserEntity TestEntity = new()
+    {
+        Id = 1,
+        FirstName = "testUser",
+        LastName = "testUser",
+        Email = "test@email.com",
+        PasswordHash = "$2a$11$8STxovb1zSpiMqGkxOcJbOkKvvUTYWWEE2dW92mmpZ1JvPhDUJwjO"
+    };
 
     public TestRoleService()
     {
+        _mockConfiguration.Setup(config => config[TokenConfigName])
+            .Returns(TestToken);
+        _tokenService = new TokenService(_mockConfiguration.Object);
+        _jwtToken = _tokenService.CreateToken(TestEntity, "user");
+
+        _mockHttpRequest.Setup(r =>
+                r.Headers[HeaderNames.Authorization])
+            .Returns(new Microsoft.Extensions.Primitives.StringValues("Bearer thisIsAFakeToken"));
+
+        _mockHttpContext.SetupGet(c =>
+                c.Request)
+            .Returns(_mockHttpRequest.Object);
         Mock<DatabaseContext> mockDatabaseContext = new();
         RoleService = new RoleService(
             mockDatabaseContext.Object,
-            new TokenService(_mockConfiguration.Object)
+            _tokenService
         );
 
         mockDatabaseContext.Setup(x => x.RoleEntities)
@@ -40,7 +68,7 @@ public class TestRoleService
         var result = await RoleService
             .RoleExists(
                 TestDataHelper.GetFakeRolesList().First().RoleName
-                );
+            );
         result.Should().NotBeNull();
         result.Should().BeOfType<RoleEntity>();
     }
